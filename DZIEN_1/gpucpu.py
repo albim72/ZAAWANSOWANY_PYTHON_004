a647bca5bc40b8f106f0ce7bf42e6ee363310810
albim72 <marcin.albiniak72@gmail.com>
2023-04-05 13:03:02 +0200

    Create gpucpu.py

---
+++ DZIEN_1/gpucpu.py
@@ -0,0 +1,25 @@
+from numba import jit,cuda
+import numpy as np
+from timeit import default_timer as timer
+
+def cpu_function(a):
+    for i in range(10_000_000):
+        a[i] += 1
+
+@jit(target_backend='cuda')
+def gpu_function(a):
+    for i in range(10_000_000):
+        a[i] += 1
+
+
+if __name__ == '__main__':
+    n = 10_000_000
+    a = np.ones(n,dtype=np.float64)
+
+    start = timer()
+    cpu_function(a)
+    print(f"funkcja wykonana na CPU: {timer()-start} s")
+
+    start = timer()
+    gpu_function(a)
+    print(f"funkcja wykonana na GPU: {timer() - start} s")
