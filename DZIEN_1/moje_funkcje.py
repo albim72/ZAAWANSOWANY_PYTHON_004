81910a7c3f49c5a53e4689b3a34b12be9cc41a1c
albim72 <marcin.albiniak72@gmail.com>
2023-04-05 10:20:37 +0200

    Update moje_funkcje.py

--- DZIEN_1/moje_funkcje.py
+++ DZIEN_1/moje_funkcje.py
@@ -1,1 +1,26 @@
+#przykład 1
+
 num = [67,8,9,-1,0,34,55,100,-99,35,67,99,2,8,12]
+
+kr = [(9,3),(6,90),(45,3)]
+
+numparz = list(filter(lambda x:x%2==0,num))
+print(numparz)
+
+cube = list(map(lambda x:x**3,num))
+print(cube)
+
+def multifive(n):
+    return n*5
+
+five = list(map(multifive,num))
+
+print(five)
+
+def multifiveplus(n):
+    return n[0]*5+n[1]
+
+dwa = list(map(multifiveplus,kr))
+print(dwa)
+
+
