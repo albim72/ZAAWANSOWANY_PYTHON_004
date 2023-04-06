8d199c2586b6c16d24de3241c22312172ced1972
albim72 <marcin.albiniak72@gmail.com>
2023-04-06 13:46:02 +0200

    Create metaperson.py

---
+++ DZIEN_2/metaperson.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass,Field
+from datetime import  datetime
+
+#Field - default, default_factory, init, repr, hash, compare, metadata,kw_only
+
+params = {
+    'firstname':Field(None,None,True,True,True,True,None,None),
+    'lastname':Field(None,None,True,True,True,True,None,None),
+    'year_of_birth':Field(None,None,True,True,True,True,None,None)
+}
+
+def age(self):
+    return datetime.now().year - self.year_of_birth
+
+MetaPerson = dataclass(type('MetaPerson',(),{'__annotations__':params,'age':property(age)}))
+
+p = MetaPerson('Romualda','Tracz',1954)
+print(p)
+print(type(p))
+print(p.age)
